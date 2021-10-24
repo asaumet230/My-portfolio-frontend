021c8b1883f0dff280d9cf8b45b0c9bb08b00509
Andres Felipe Saumet <andressaumet@gmail.com>
2021-10-24 06:48:04 -0500

    This portfolio content gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,5 +54,22 @@ module.exports = {
         collectionTypes: [`Projects`, `Skills`,`Experiences`,`Testimonials`, `Articles`, `Terms`],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-J7N5KM5X7E", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    `gatsby-plugin-sitemap`
   ]
 }
